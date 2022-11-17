47bcfa6f5c1afdd871974949ad6029d86d674c08
Oliver Atkins <ollieatkins8@outlook.com>
2022-11-17 12:17:50 +0000

    everything up to TDD, our first test has failed, now need to code the feature

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -4,10 +4,36 @@ class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url # "/"
 
-    save_and_open_screenshot # open image viewer of what your automated browser sees
+    # save_and_open_screenshot # open image viewer of what your automated browser sees
     assert_selector "h1", text: "Awesome Products"
   end
+
+  test "same number of cards displayed as products in db" do
+    visit root_url
+    assert_selector ".card-product", count: Product.count
+  end
+
+  test "lets a signed in user create a new product" do
+    login_as users(:george) # Warden::TestHelpers (see test_helper.rb)
+    visit "/products/new"
+    # save_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    # save_screenshot
+
+    click_on "Create Product"
+    # save_screenshot
+
+    # should be directed back to the Home with a new product
+    assert_text "Change your life: Learn to code"
+  end
+
 end
+
+
+
+# NOTES 1
 
 # the syntax we use for testing
 # test - similar to def
@@ -15,3 +41,15 @@ end
 # assert_selector - capybara - makes sure there is a h1 with the text "Awesome Products" on the page
 # if the asset selector returns true - the test has passed
 # little green dot means that you passed the test
+
+
+
+# NOTES 4
+
+# better to add nultiple methods test methods rather than multiple assertions in one test
+
+
+
+# NOTES 6
+
+# YOU NEEED TO WRITE A TEST THAT FAILS (AT FIRST)
